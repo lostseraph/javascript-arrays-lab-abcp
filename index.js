0cd362a8d3da717692bad35397fe43520f99f3a4
Roscoe Sabale <rsabale43@gmail.com>
2018-01-19 19:28:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return
+}
+
+function appendKitten(name) {
+  var kittens2 = [...kittens, name]
+  return kittens2
+}
+
+function prependKitten(name) {
+  var kittens2 = [name, ...kittens]
+  return kittens2
+}
+
+function removeLastKitten() {
+  var kittens2 = [...kittens]
+  kittens2.pop()
+  return kittens2
+}
+
+function removeFirstKitten() {
+  var kittens2 = [...kittens]
+  kittens2.shift()
+  return kittens2
+}
